3bab4cf4811e4d7e20b99af701e71ee210bd5793
ChrisRyanTang <ctang93@my.bcit.ca>
2024-11-29 15:26:23 -0800

    changed app.py code

--- Processing_Service/app.py
+++ Processing_Service/app.py
@@ -4,21 +4,34 @@ import yaml
 import logging
 import logging.config
 import connexion
+import os
 from connexion.middleware import MiddlewarePosition
 from starlette.middleware.cors import CORSMiddleware
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
 
-with open('app_conf.yml', 'r') as f:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+# Load application configuration
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-
-with open('log_conf.yml', 'r') as f:
+# Load logging configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
-
+# Set up logger
 logger = logging.getLogger('basicLogger')
+logger.info("App Conf File: %s", app_conf_file)
+logger.info("Log Conf File: %s", log_conf_file)
 
 def populate_stats():
     logger.info('Starting periodic processing')
--- Storage/app.py
+++ Storage/app.py
@@ -4,6 +4,7 @@ import logging
 import logging.config
 import json
 import time
+import os
 from threading import Thread
 from kafka import KafkaProducer
 from pykafka import KafkaClient
@@ -18,14 +19,28 @@ from get_all_reviews import Review
 from rating_game import Rating
 
 
-with open('app_conf.yml', 'r') as f:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+# Load application configuration
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-with open('log_conf.yml', 'r') as f:
+# Load logging configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
+# Set up logger
 logger = logging.getLogger('basicLogger')
+logger.info("App Conf File: %s", app_conf_file)
+logger.info("Log Conf File: %s", log_conf_file)
 
 
 DB_ENGINE = create_engine(
