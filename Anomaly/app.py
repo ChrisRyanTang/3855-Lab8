fb109b0da9dfe015cdb1399b7a3380cc4043c477
ChrisRyanTang <ctang93@my.bcit.ca>
2024-11-29 15:55:39 -0800

    changed anomaly

--- Anomaly/app.py
+++ Anomaly/app.py
@@ -2,22 +2,35 @@ import connexion
 import json
 import yaml
 import logging.config
+import os
 from connexion.middleware import MiddlewarePosition
 from starlette.middleware.cors import CORSMiddleware
 from pykafka import KafkaClient
 from pykafka.common import OffsetType
 from datetime import datetime
 
-# Load configurations
-with open('app_conf.yml', 'r') as f:
-    app_config = yaml.safe_load(f)
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
 
-with open('log_conf.yml', 'r') as f:
+# Load application configuration
+with open(app_conf_file, 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+# Load logging configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
+# Set up logger
 logger = logging.getLogger('basicLogger')
-
+logger.info("App Conf File: %s", app_conf_file)
+logger.info("Log Conf File: %s", log_conf_file)
 # Kafka setup
 hostname = f"{app_config['events']['hostname']}:{app_config['events']['port']}"
 client = KafkaClient(hosts=hostname)
