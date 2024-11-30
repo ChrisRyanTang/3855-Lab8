c184fbc1c6f840ae4219d7f9d44f2f6ee8f62e58
ChrisRyanTang <ctang93@my.bcit.ca>
2024-11-29 18:07:11 -0800

    os.path

--- Anomaly/app.py
+++ Anomaly/app.py
@@ -39,6 +39,20 @@ topic = client.topics[str.encode(app_config["events"]["topic"])]
 # JSON data store
 DATA_STORE = app_config['datastore']['filepath']
 
+def ensure_datastore():
+    """Ensure the datastore file and folder exist."""
+    # Get the folder path from the file path
+    folder_path = os.path.dirname(DATA_STORE)
+    
+    # Create the anomalies.json file if it doesn't exist
+    if not os.path.exists(DATA_STORE):
+        with open(DATA_STORE, 'w') as f:
+            json.dump([], f, indent=4)  # Initialize with an empty list
+        logger.info(f"Created file: {DATA_STORE}")
+
+# Call this function at the start of your app
+ensure_datastore()
+
 def get_kafka_consumer():
     """Initialize Kafka consumer."""
     consumer = topic.get_simple_consumer(
