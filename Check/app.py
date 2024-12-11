e2d09e2b0e76719a92140a4656fcf2cc68759a56
ChrisRyanTang <ctang93@my.bcit.ca>
2024-12-11 12:20:55 -0800

    :(

--- Check/app.py
+++ Check/app.py
@@ -91,7 +91,22 @@ def get_status():
     
 
 def get_event_stats():
-    pass
+    client = KafkaClient(hosts=f"{app_config['events']['hostname']}:{app_config['events']['port']}")
+    topic = client.topics[str.encode(app_config['events']['topic'])]
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=1000)
+    num_reviews, num_ratings = 0, 0
+    try:
+        for msg in consumer:
+            msg_str = msg.value.decode('utf-8')
+            event = json.loads(msg_str)
+            if event['type'] == 'get_all_reviews':
+                num_reviews += 1
+            elif event['type'] == 'rating_game':
+                num_ratings += 1
+    except Exception as e:
+        logger.error(f"Error retrieving event stats: {str(e)}")
+        return {"message": "Error retrieving event stats"}, 404
+    return {"num_reviews": num_reviews, "num_ratings": num_ratings}, 201
 
 app = connexion.FlaskApp(__name__, specification_dir='.')
 app.add_api('openapi.yaml')
